a99f302e50f5e429826c51bce9cc72bf582bc72e
Fran <fra.quintana@duocuc.cl>
2025-11-16 22:12:31 -0300

    Reapply "arreglamos base de datos"
    This reverts commit b9f4824677ec0b9d96f2a5d1b5b48cb72651b576.

--- app/src/main/java/com/example/rentfage/data/local/entity/CasaEntity.kt
+++ app/src/main/java/com/example/rentfage/data/local/entity/CasaEntity.kt
@@ -1,4 +1,4 @@
-package com.example.rentfage.data.local.room.entity
+package com.example.rentfage.data.local.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
--- app/src/main/java/com/example/rentfage/data/repository/CasasRepository.kt
+++ app/src/main/java/com/example/rentfage/data/repository/CasasRepository.kt
@@ -1,8 +1,7 @@
 package com.example.rentfage.data.repository
 
-import com.example.rentfage.data.local.casasDeEjemplo
-import com.example.rentfage.data.local.room.dao.CasaDao
-import com.example.rentfage.data.local.room.entity.CasaEntity
+import com.example.rentfage.data.local.dao.CasaDao
+import com.example.rentfage.data.local.entity.CasaEntity
 import kotlinx.coroutines.flow.Flow
 
 // Repositorio para manejar los datos de las casas.
@@ -27,24 +26,5 @@ class CasasRepository(private val casaDao: CasaDao) {
 
     suspend fun borrarCasa(casa: CasaEntity) {
         casaDao.borrar(casa)
-    }
-
-    // --- LOGICA DE INICIALIZACION ---
-    suspend fun popularBaseDeDatosSiEsNecesario() {
-        if (casaDao.count() == 0) {
-            val casasEntity = casasDeEjemplo.map { casa ->
-                CasaEntity(
-                    id = casa.id,
-                    price = casa.price,
-                    address = casa.address,
-                    details = casa.details,
-                    imageUri = casa.imageUri,
-                    latitude = casa.latitude,
-                    longitude = casa.longitude,
-                    isFavorite = casa.isFavorite
-                )
-            }
-            casaDao.insertarTodas(casasEntity)
-        }
     }
 }
